2fbaab5cb581273bb1b9ded219fc3db69ea078a3
Kai Nacke <kai@redstar.de>
2021-11-17 19:44:49 -0500

    [m88k] Implement computeHostNumPhysicalCores
    for OpenBSD and FreeBSD.

--- llvm/lib/Support/Host.cpp
+++ llvm/lib/Support/Host.cpp
@@ -1515,7 +1515,8 @@ int computeHostNumPhysicalCores() {
   }
   return -1;
 }
-#elif defined(__linux__) && defined(__s390x__)
+#elif (defined(__linux__) && defined(__s390x__)) || defined(__OpenBSD__) ||    \
+    defined(__FreeBSD__)
 int computeHostNumPhysicalCores() { return sysconf(_SC_NPROCESSORS_ONLN); }
 #elif defined(__APPLE__)
 // Gets the number of *physical cores* on the machine.
