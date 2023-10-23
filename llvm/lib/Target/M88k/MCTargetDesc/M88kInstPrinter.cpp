b3308c2c0d28abff6fd34e79da176ff36df91a1b
Kai Nacke <kai@redstar.de>
2023-10-23 07:39:09 -0400

    [m88k] More clang-tidy updates.

--- llvm/lib/Target/M88k/MCTargetDesc/M88kInstPrinter.cpp
+++ llvm/lib/Target/M88k/MCTargetDesc/M88kInstPrinter.cpp
@@ -11,7 +11,6 @@
 #include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCInst.h"
 #include "llvm/MC/MCSymbol.h"
-#include "llvm/Support/Casting.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/MathExtras.h"
 #include "llvm/Support/raw_ostream.h"
