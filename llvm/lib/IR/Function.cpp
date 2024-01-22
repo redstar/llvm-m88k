bbce66ad4edfcf5f9a13dfd25edab2b3396aa043
Kai Nacke <kai@redstar.de>
2024-01-21 22:03:19 -0500

    [m88k] Define intrinsic llvm.m88k.ff1/ff0
    Adds intrinsic llvm.m88k.ff1, which lowers into instruction ff1, and
    intrinsic llvm.m88k.ff0, which lowers into instruction ff0.
    Currently it seems that this intrinsic could be useful in some situations.
    A later commit will expose those intrinsics in clang.

--- llvm/lib/IR/Function.cpp
+++ llvm/lib/IR/Function.cpp
@@ -38,6 +38,7 @@
 #include "llvm/IR/IntrinsicsDirectX.h"
 #include "llvm/IR/IntrinsicsHexagon.h"
 #include "llvm/IR/IntrinsicsLoongArch.h"
+#include "llvm/IR/IntrinsicsM88k.h"
 #include "llvm/IR/IntrinsicsMips.h"
 #include "llvm/IR/IntrinsicsNVPTX.h"
 #include "llvm/IR/IntrinsicsPowerPC.h"
