66211e3ba039d7345cf9eb8f7844d60e897a309b
Kai Nacke <kai@redstar.de>
2024-02-26 08:39:51 -0500

    [m88k] Implement G_VASTART

--- llvm/lib/Target/M88k/M88kMachineFunctionInfo.h
+++ llvm/lib/Target/M88k/M88kMachineFunctionInfo.h
@@ -24,6 +24,16 @@ class M88kMachineFunctionInfo : public MachineFunctionInfo {
   /// stored.
   int FramePointerIndex = 0;
 
+  /// Number of first variadic register, counted from 0.
+  int VarArgsFirstReg = 0;
+
+  /// FrameIndex for start of varargs area for arguments passed in
+  /// general purpose registers.
+  int VarArgsRegIndex = 0;
+
+  /// FrameIndex for start of argument area.
+  int StackIndex = 0;
+
 public:
   M88kMachineFunctionInfo(const Function &F, const TargetSubtargetInfo *STI) {}
 
@@ -34,6 +44,15 @@ public:
 
   int getFramePointerIndex() const { return FramePointerIndex; }
   void setFramePointerIndex(int Idx) { FramePointerIndex = Idx; }
+
+  int getVarArgsFirstReg() const { return VarArgsFirstReg; }
+  void setVarArgsFirstReg(int RegNo) { VarArgsFirstReg = RegNo; }
+
+  int getVarArgsRegIndex() const { return VarArgsRegIndex; }
+  void setVarArgsRegIndex(int Idx) { VarArgsRegIndex = Idx; }
+
+  int getStackIndex() const { return StackIndex; }
+  void setStackIndex(int Idx) { StackIndex = Idx; }
 };
 } // end namespace llvm
 
