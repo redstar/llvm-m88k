d70f44059409bb38c435e45e186dbfe92c7d2c7b
Kai Nacke <kai.nacke@redstar.de>
2024-02-28 10:11:31 -0500

    [m88k][clang] Implement va_next
    Adds an implementation of va_next to the clang ABI.

--- clang/lib/CodeGen/Targets/M88k.cpp
+++ clang/lib/CodeGen/Targets/M88k.cpp
@@ -19,29 +19,90 @@ using namespace clang::CodeGen;
 //===----------------------------------------------------------------------===//
 
 namespace {
-class M88kABIInfo final : public ABIInfo {
-  DefaultABIInfo defaultInfo;
+class M88kABIInfo final : public DefaultABIInfo {
 
 public:
-  explicit M88kABIInfo(CodeGen::CodeGenTypes &CGT)
-      : ABIInfo(CGT), defaultInfo(CGT) {}
+  explicit M88kABIInfo(CodeGen::CodeGenTypes &CGT) : DefaultABIInfo(CGT) {}
 
-  void computeInfo(CodeGen::CGFunctionInfo &FI) const override {}
+  // void computeInfo(CodeGen::CGFunctionInfo &FI) const override {}
 
   CodeGen::Address EmitVAArg(CodeGen::CodeGenFunction &CGF,
                              CodeGen::Address VAListAddr,
                              QualType Ty) const override {
-    return VAListAddr;
+    // Assume that va_list type is correct; should be pointer to LLVM type:
+    // struct {
+    //    int __va_arg;
+    //    int *__va_stk;
+    //    int *__va_reg;
+    // };
+
+    // How are structures handled?
+
+    // Align the argument
+    CharUnits PtrAlign = CharUnits::fromQuantity(4);
+    CharUnits DirectAlign = CharUnits::fromQuantity(8);
+    CharUnits SlotSize = CharUnits::fromQuantity(4);
+
+    Ty = getContext().getCanonicalType(Ty);
+    auto TyInfo = getContext().getTypeInfoInChars(Ty);
+    llvm::Type *ArgTy = CGF.ConvertTypeForMem(Ty);
+    llvm::Type *DirectTy = ArgTy;
+    CharUnits DirectSize = TyInfo.Width;
+
+    // Decide which pointer to use: __va_arg < 8 ? __va_reg : __va_stk.
+    Address VaArgPtr =
+        CGF.Builder.CreateStructGEP(VAListAddr, 0, "__va_arg.addr");
+    llvm::Value *VaArg = CGF.Builder.CreateLoad(VaArgPtr, "__va_arg");
+    llvm::Value *MaxRegs = llvm::ConstantInt::get(CGF.Int32Ty, 8);
+    llvm::Value *InRegs =
+        CGF.Builder.CreateICmpULT(VaArg, MaxRegs, "fits_in_regs");
+
+    llvm::BasicBlock *InRegBlock = CGF.createBasicBlock("vaarg.in_reg");
+    llvm::BasicBlock *InStkBlock = CGF.createBasicBlock("vaarg.in_stk");
+    llvm::BasicBlock *ContBlock = CGF.createBasicBlock("vaarg.end");
+    CGF.Builder.CreateCondBr(InRegs, InRegBlock, InStkBlock);
+
+    // Value comes from register.
+    CGF.EmitBlock(InRegBlock);
+    Address VaRegPtr =
+        CGF.Builder.CreateStructGEP(VAListAddr, 2, "__va_reg.addr");
+    Address VaReg = Address(CGF.Builder.CreateLoad(VaRegPtr, "__va_reg"),
+                            CGF.Int8Ty, PtrAlign);
+    CGF.Builder.CreateBr(ContBlock);
+
+    // Value comes from stack.
+    CGF.EmitBlock(InStkBlock);
+    Address VaStkPtr =
+        CGF.Builder.CreateStructGEP(VAListAddr, 1, "__va_stk.addr");
+    Address VaStk = Address(CGF.Builder.CreateLoad(VaStkPtr, "__va_stk"),
+                            CGF.Int8Ty, PtrAlign);
+    CGF.Builder.CreateBr(ContBlock);
+
+    CGF.EmitBlock(ContBlock);
+    Address VaAddr =
+        emitMergePHI(CGF, VaReg, InRegBlock, VaStk, InStkBlock, "va_arg.addr");
+
+    // Load value and increment pointer.
+    Address ResAddr =
+        emitVoidPtrDirectVAArg(CGF, VaAddr, DirectTy, DirectSize, DirectAlign,
+                               SlotSize, /*AllowHigherAlign=*/true,
+                               /*ForceRightAdjust=*/true);
+
+    // Increment and store __va_arg.
+    llvm::Value *One = llvm::ConstantInt::get(CGF.Int32Ty, 1);
+    llvm::Value *NewVaArg = CGF.Builder.CreateAdd(VaArg, One, "__va_arg.inc");
+    CGF.Builder.CreateStore(NewVaArg, VaArgPtr);
+
+    return ResAddr;
   }
 };
 
 class M88kTargetCodeGenInfo final : public TargetCodeGenInfo {
 public:
   explicit M88kTargetCodeGenInfo(CodeGen::CodeGenTypes &CGT)
-      //: TargetCodeGenInfo(std::make_unique<M88kABIInfo>(CGT)) {}
-      : TargetCodeGenInfo(std::make_unique<DefaultABIInfo>(CGT)) {}
+      : TargetCodeGenInfo(std::make_unique<M88kABIInfo>(CGT)) {}
 };
-}
+} // namespace
 
 std::unique_ptr<TargetCodeGenInfo>
 CodeGen::createM88kTargetCodeGenInfo(CodeGenModule &CGM) {
